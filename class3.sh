f65009a18bcf3e90055f76dd5ae15b2760122f13
fejxc <1031364436@qq.com>
2021-05-25 19:08:14 +0800

    增加循环遍历整个班级

--- class3.sh
+++ class3.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 count=0
-id=1809103015
+id=1809103001
 
 find . -name "18-3*.csv" > filePath.log 
-
 function countById() {
     cat filePath.log | while read line
     do
@@ -11,13 +10,23 @@ function countById() {
     if [ $? == 0 ];
     then
         #echo 'find'
-        count=$[ $count + 1 ]
+        #count=$[ $count + 1 ]
+        let "count++"
         #echo $id'共出勤'$count'次'
     else
     echo "班上没有改学号的同学！请检查学号是否输入正确！" 
     fi
     echo $id'共出勤'$count'次' > totolResult.log
-    cat totolResult.log
-done
+    
+    done
 }
-countById
+while (($id<=1809103054)) 
+do
+    countById
+    cat totolResult.log >> resForClass3.log
+    let "id++"
+done 
+
+
+
+
